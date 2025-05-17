279ab143b3c903679c7a2343a4fad712f08fb002
root <root@bistroalpasoar.com>
2025-05-17 21:08:20 +0000

    Funcionamiento en servidores

--- frontend/src/helpers/SocketProvider.tsx
+++ frontend/src/helpers/SocketProvider.tsx
@@ -18,7 +18,7 @@ export const SocketProvider = ({ children }: SocketProviderProps) => {
     const location = useLocation();
 
     useEffect(() => {
-        const socketInstance = io("https://bistroalpasoar.com/ws", {
+        const socketInstance = io("https://bistroalpasoar.com", {
             path: "/ws/socket.io",
             transports: ["websocket"],
             reconnectionAttempts: 3,
