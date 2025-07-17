a2e52cdedc1cf82a4c54613ceea57de1d4a6f8f5
root <root@bistroalpasoar.com>
2025-07-17 17:49:15 +0000

    Update dish translation response to include description and enhance ShoppingCart dish detail updates

--- frontend/src/pages/web/DishSelected.tsx
+++ frontend/src/pages/web/DishSelected.tsx
@@ -43,7 +43,7 @@ export const DishSelected = () => {
             setDish(dish);
         };
         fetchDish();
-    }, [dishId]);
+    }, [dishId, language]);
 
     useEffect(() => {
         const fetchAllergens = async () => {
